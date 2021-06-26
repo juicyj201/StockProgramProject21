2d0da98077a566e5941611e397373cd6b43c9299
Joshua Julies <220102473@mycput.ac.za>
2021-06-26 21:55:32 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace StockProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stockView = new System.Windows.Forms.DataGridView();
+            this.stockBtn = new System.Windows.Forms.Button();
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.sortNameBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.stockView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stockView
+            // 
+            this.stockView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.stockView.Location = new System.Drawing.Point(46, 146);
+            this.stockView.Name = "stockView";
+            this.stockView.RowHeadersWidth = 51;
+            this.stockView.RowTemplate.Height = 24;
+            this.stockView.Size = new System.Drawing.Size(1279, 422);
+            this.stockView.TabIndex = 0;
+            // 
+            // stockBtn
+            // 
+            this.stockBtn.Location = new System.Drawing.Point(46, 42);
+            this.stockBtn.Name = "stockBtn";
+            this.stockBtn.Size = new System.Drawing.Size(319, 60);
+            this.stockBtn.TabIndex = 1;
+            this.stockBtn.Text = "Show stock";
+            this.stockBtn.UseVisualStyleBackColor = true;
+            this.stockBtn.Click += new System.EventHandler(this.stockBtn_Click);
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.Location = new System.Drawing.Point(393, 44);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(305, 57);
+            this.sortBtn.TabIndex = 4;
+            this.sortBtn.Text = "Sort stock";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            this.sortBtn.Click += new System.EventHandler(this.sortBtn_Click);
+            // 
+            // sortNameBtn
+            // 
+            this.sortNameBtn.Location = new System.Drawing.Point(729, 42);
+            this.sortNameBtn.Name = "sortNameBtn";
+            this.sortNameBtn.Size = new System.Drawing.Size(305, 57);
+            this.sortNameBtn.TabIndex = 5;
+            this.sortNameBtn.Text = "Sort stock by Name";
+            this.sortNameBtn.UseVisualStyleBackColor = true;
+            this.sortNameBtn.Click += new System.EventHandler(this.sortNameBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1379, 600);
+            this.Controls.Add(this.sortNameBtn);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.stockBtn);
+            this.Controls.Add(this.stockView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.stockView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView stockView;
+        private System.Windows.Forms.Button stockBtn;
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.Button sortNameBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace StockProgram
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        private SQLiteConnection conn = new SQLiteConnection(@"data source=C:\Users\joshu\source\repos\StockProgram\StockDatabase.sqlite");
+        private DataTable table = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openConnection();
+            closeConnection();
+        }
+
+        private void openConnection() { 
+            conn.Open();
+        }
+
+        private void closeConnection() {
+            conn.Close();
+        }
+
+        private void stockBtn_Click(object sender, EventArgs e)
+        {
+            //the command object
+            string selectStock = "SELECT* from Products";
+            SQLiteCommand selectComm = new SQLiteCommand(selectStock, conn);
+
+            //the adapter
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(selectComm);
+            adapter.Fill(table);
+
+            //actually putting it in the form
+            stockView.DataSource = table;
+        }
+
+        private void sortBtn_Click(object sender, EventArgs e)
+        {
+            //the command object
+            string sortStock = "SELECT* from Products ORDER BY ProductId";
+            SQLiteCommand sortComm = new SQLiteCommand(sortStock, conn);
+
+            //the adapter
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sortComm);
+            adapter.Fill(table);
+
+            //actually putting it in the form
+            stockView.DataSource = table;
+        }
+
+        private void sortNameBtn_Click(object sender, EventArgs e)
+        {
+            //the command object
+            string sortStock = "SELECT* from Products ORDER BY Name";
+            SQLiteCommand sortNameComm = new SQLiteCommand(sortStock, conn);
+
+            //the adapter
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sortNameComm);
+            adapter.Fill(table);
+
+            //actually putting it in the form
+            stockView.DataSource = table;
+        }
+    }
+}

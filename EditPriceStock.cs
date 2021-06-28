c331e4850935d226f8ab3b4a287103a28dec2df3
juicyj201 <80794017+juicyj201@users.noreply.github.com>
2021-06-28 12:55:32 +0200

    Add EditPriceStock form (SERGE)

---
+++ EditPriceStock.cs
@@ -0,0 +1,21 @@
+﻿//SERGE
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockProgram
+{
+    public partial class EditPriceStock : Form
+    {
+        public EditPriceStock()
+        {
+            InitializeComponent();
+        }
+    }
+}

6611cf4109f91479b1237dee0f794076a2679e07
Joshua Julies <80794017+juicyj201@users.noreply.github.com>
2021-08-07 15:46:22 +0200

    Update edit items method in main database

--- MainDatabase.cs
+++ MainDatabase.cs
@@ -62,9 +62,8 @@ namespace StockProgram
             stockView.DataSource = table;
         }
 
-        //method/s for editing the datatable and allowing the database to be change in turn
         private void EditItems() {
-            
+            //TODO add code in that will allow the user to add items in
         }
 
         //methods for initialising the connection and table variables

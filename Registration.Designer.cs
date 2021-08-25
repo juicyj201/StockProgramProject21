932d3bbd95a2e360d91a40e62d1fe2f2f118cfa6
Matthew Jones <220077681@mycput.ac.za>
2021-08-25 14:36:42 +0200

    registration page complete.

--- Registration.Designer.cs
+++ Registration.Designer.cs
@@ -29,12 +29,168 @@ namespace StockProgram
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.RegisterBtn = new System.Windows.Forms.Button();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtCPass = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCPass = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(532, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.lblCPass);
+            this.panel2.Controls.Add(this.lblName);
+            this.panel2.Controls.Add(this.txtCPass);
+            this.panel2.Controls.Add(this.txtPass);
+            this.panel2.Controls.Add(this.RegisterBtn);
+            this.panel2.Controls.Add(this.lblPass);
+            this.panel2.Controls.Add(this.lblEmail);
+            this.panel2.Controls.Add(this.txtEmail);
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Location = new System.Drawing.Point(1, 56);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(532, 305);
+            this.panel2.TabIndex = 2;
+            // 
+            // RegisterBtn
+            // 
+            this.RegisterBtn.BackColor = System.Drawing.Color.Gray;
+            this.RegisterBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterBtn.Location = new System.Drawing.Point(381, 244);
+            this.RegisterBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.RegisterBtn.Name = "RegisterBtn";
+            this.RegisterBtn.Size = new System.Drawing.Size(96, 50);
+            this.RegisterBtn.TabIndex = 4;
+            this.RegisterBtn.Text = "SIGN UP";
+            this.RegisterBtn.UseVisualStyleBackColor = false;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(39, 135);
+            this.lblPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(86, 20);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Password";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(39, 86);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(207, 83);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(270, 26);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(207, 37);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(270, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(207, 132);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(270, 26);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // txtCPass
+            // 
+            this.txtCPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPass.Location = new System.Drawing.Point(207, 180);
+            this.txtCPass.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCPass.Name = "txtCPass";
+            this.txtCPass.Size = new System.Drawing.Size(270, 26);
+            this.txtCPass.TabIndex = 6;
+            this.txtCPass.UseSystemPasswordChar = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(39, 40);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(89, 20);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Full Name";
+            // 
+            // lblCPass
+            // 
+            this.lblCPass.AutoSize = true;
+            this.lblCPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPass.Location = new System.Drawing.Point(39, 183);
+            this.lblCPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCPass.Name = "lblCPass";
+            this.lblCPass.Size = new System.Drawing.Size(153, 20);
+            this.lblCPass.TabIndex = 8;
+            this.lblCPass.Text = "Confirm Password";
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 361);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Registration";
             this.Text = "Registration";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button RegisterBtn;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblCPass;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtCPass;
+        private System.Windows.Forms.TextBox txtPass;
     }
 }
--- Registration.cs
+++ Registration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace StockProgram
 {
@@ -15,6 +16,41 @@ namespace StockProgram
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private void CloseForm()
+        {
+            this.Hide();
+
+        }
+
+        SQLiteConnection conn = new SQLiteConnection("Data Source = Login.db; Version = 3;");
+        SQLiteCommand cmd = new SQLiteCommand();
+
+        private void RegisterBtn_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" && txtEmail.Text == "" && txtPass.Text == "" && txtCPass.Text == "")
+            {
+                MessageBox.Show("Fields are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPass.Text == txtCPass.Text)
+            {
+                conn.Open();
+                string register = "INSERT INTO users VALUES('" + txtName.Text + "', '" + txtEmail.Text + "', '" + txtPass.Text + "')";
+                cmd = new SQLiteCommand(register, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+
+                MessageBox.Show("Your account has been successfully created", "Registration Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else 
+            {
+                MessageBox.Show("Passwords do not match, Please Re-enter", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPass.Text = "";
+                txtCPass.Text = "";
+                txtPass.Focus();
+            }
         }
     }
 }

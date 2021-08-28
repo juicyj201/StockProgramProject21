528ff59b68b6bc93de3167ecb02b5e8033d51bfb
juicyj201 <220102473@mycput.ac.za>
2021-08-29 00:00:22 +0200

    Add button function in updatedatabaseview

--- UserControls/UpdateDatabaseView.Designer.cs
+++ UserControls/UpdateDatabaseView.Designer.cs
@@ -49,71 +49,67 @@ namespace StockProgram.UserControls
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(64, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(83, 22);
-            this.textBox1.TabIndex = 0;
-            // 
-            // UpdateDatabaseView
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox1);
-            this.Name = "UpdateDatabaseView";
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(188, 83);
+            this.textBox1.Location = new System.Drawing.Point(251, 102);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 26);
+            this.textBox1.Size = new System.Drawing.Size(297, 30);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(188, 131);
+            this.textBox2.Location = new System.Drawing.Point(251, 161);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(224, 26);
+            this.textBox2.Size = new System.Drawing.Size(297, 30);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(188, 182);
+            this.textBox3.Location = new System.Drawing.Point(251, 224);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(224, 26);
+            this.textBox3.Size = new System.Drawing.Size(297, 30);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(188, 231);
+            this.textBox4.Location = new System.Drawing.Point(251, 284);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(224, 26);
+            this.textBox4.Size = new System.Drawing.Size(297, 30);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(188, 280);
+            this.textBox5.Location = new System.Drawing.Point(251, 345);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(224, 26);
+            this.textBox5.Size = new System.Drawing.Size(297, 30);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(188, 330);
+            this.textBox6.Location = new System.Drawing.Point(251, 406);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(224, 26);
+            this.textBox6.Size = new System.Drawing.Size(297, 30);
             this.textBox6.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 88);
+            this.label1.Location = new System.Drawing.Point(36, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 19);
+            this.label1.Size = new System.Drawing.Size(108, 24);
             this.label1.TabIndex = 6;
             this.label1.Text = "Product Id";
             // 
@@ -121,9 +117,10 @@ namespace StockProgram.UserControls
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 136);
+            this.label2.Location = new System.Drawing.Point(36, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 19);
+            this.label2.Size = new System.Drawing.Size(143, 24);
             this.label2.TabIndex = 7;
             this.label2.Text = "Product Name";
             // 
@@ -131,9 +128,10 @@ namespace StockProgram.UserControls
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(27, 187);
+            this.label3.Location = new System.Drawing.Point(36, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 19);
+            this.label3.Size = new System.Drawing.Size(137, 24);
             this.label3.TabIndex = 8;
             this.label3.Text = "Product Price";
             // 
@@ -141,9 +139,10 @@ namespace StockProgram.UserControls
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(27, 236);
+            this.label4.Location = new System.Drawing.Point(36, 290);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 19);
+            this.label4.Size = new System.Drawing.Size(166, 24);
             this.label4.TabIndex = 9;
             this.label4.Text = "Product Quantity";
             // 
@@ -151,9 +150,10 @@ namespace StockProgram.UserControls
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(27, 285);
+            this.label5.Location = new System.Drawing.Point(36, 351);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 19);
+            this.label5.Size = new System.Drawing.Size(132, 24);
             this.label5.TabIndex = 10;
             this.label5.Text = "Date Of Use";
             // 
@@ -161,9 +161,10 @@ namespace StockProgram.UserControls
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(27, 335);
+            this.label6.Location = new System.Drawing.Point(36, 412);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(147, 19);
+            this.label6.Size = new System.Drawing.Size(181, 24);
             this.label6.TabIndex = 11;
             this.label6.Text = "Date Of Purchase";
             // 
@@ -171,9 +172,10 @@ namespace StockProgram.UserControls
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(128, 25);
+            this.label7.Location = new System.Drawing.Point(171, 31);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(154, 27);
+            this.label7.Size = new System.Drawing.Size(189, 34);
             this.label7.TabIndex = 12;
             this.label7.Text = "Stock Control";
             // 
@@ -182,21 +184,24 @@ namespace StockProgram.UserControls
             this.button1.BackColor = System.Drawing.Color.DarkBlue;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(31, 374);
+            this.button1.Location = new System.Drawing.Point(41, 460);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 43);
+            this.button1.Size = new System.Drawing.Size(149, 53);
             this.button1.TabIndex = 13;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DarkBlue;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(164, 374);
+            this.button2.Location = new System.Drawing.Point(219, 460);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 43);
+            this.button2.Size = new System.Drawing.Size(157, 53);
             this.button2.TabIndex = 14;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = false;
@@ -207,16 +212,17 @@ namespace StockProgram.UserControls
             this.button3.BackColor = System.Drawing.Color.DarkBlue;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(301, 374);
+            this.button3.Location = new System.Drawing.Point(401, 460);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(111, 43);
+            this.button3.Size = new System.Drawing.Size(148, 53);
             this.button3.TabIndex = 15;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // UpdateDatabaseView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -234,8 +240,9 @@ namespace StockProgram.UserControls
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UpdateDatabaseView";
-            this.Size = new System.Drawing.Size(459, 455);
+            this.Size = new System.Drawing.Size(612, 560);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- UserControls/UpdateDatabaseView.cs
+++ UserControls/UpdateDatabaseView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace StockProgram.UserControls
 {
     public partial class UpdateDatabaseView : UserControl
     {
+        private SQLiteConnection conn = new SQLiteConnection(@"data source=.\StockDatabase.db");
+
         public UpdateDatabaseView()
         {
             InitializeComponent();
@@ -30,6 +33,48 @@ namespace StockProgram.UserControls
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openConn();
+
+                string sql = "insert into products values(@ProdName, @ProdPrice, @ProdQuantity, @DateOfUse, @DateOfPurchase)";
+                SQLiteCommand comm = new SQLiteCommand(sql, conn);
+
+                comm.Parameters.AddWithValue("@ProdName", textBox2.Text);
+                comm.Parameters.AddWithValue("@ProdPrice", textBox3.Text);
+                comm.Parameters.AddWithValue("@ProdQuantity", textBox4.Text);
+                comm.Parameters.AddWithValue("@DateOfUse", textBox5.Text);
+                comm.Parameters.AddWithValue("@DateOfPurchase", textBox6.Text);
+                int ok = 0;
+                ok = comm.ExecuteNonQuery();
+
+                if (ok > 0)
+                {
+                    MessageBox.Show("Item successfully added.");
+                }
+                else
+                {
+                    MessageBox.Show("No item has been added. Please try again. ");
+                }
+
+                closeConn();
+            }
+            catch (Exception ex) {
+                
+            }
+
+        }
+
+        private void openConn() {
+            conn.Open();
+        }
+
+        private void closeConn() {
+            conn.Close();
         }
     }
 }

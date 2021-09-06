237dd12920d301f5547f0c39956ce6a6d2336d6c
slid carter <cslidcoop@outlook.com>
2021-09-07 00:04:51 +0200

    Edit Item Price done

--- UserControls/EditStockPriceView.Designer.cs
+++ UserControls/EditStockPriceView.Designer.cs
@@ -30,84 +30,102 @@ namespace StockProgram.UserControls
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.cmblistId = new System.Windows.Forms.ComboBox();
+            this.txtboxPrice = new System.Windows.Forms.TextBox();
+            this.cmbx = new System.Windows.Forms.ComboBox();
             this.btnEditPrice = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblcomboTitle = new System.Windows.Forms.Label();
+            this.btnCloseApp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20.25F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(19, 10);
+            this.lblTitle.Location = new System.Drawing.Point(203, 22);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(252, 34);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Edit Product Price";
             // 
-            // textBox1
+            // txtboxPrice
             // 
-            this.textBox1.Location = new System.Drawing.Point(161, 142);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(271, 30);
-            this.textBox1.TabIndex = 2;
+            this.txtboxPrice.Location = new System.Drawing.Point(209, 240);
+            this.txtboxPrice.Multiline = true;
+            this.txtboxPrice.Name = "txtboxPrice";
+            this.txtboxPrice.Size = new System.Drawing.Size(230, 35);
+            this.txtboxPrice.TabIndex = 2;
             // 
-            // cmblistId
+            // cmbx
             // 
-            this.cmblistId.FormattingEnabled = true;
-            this.cmblistId.Location = new System.Drawing.Point(479, 69);
-            this.cmblistId.Name = "cmblistId";
-            this.cmblistId.Size = new System.Drawing.Size(230, 24);
-            this.cmblistId.TabIndex = 5;
-            this.cmblistId.SelectedIndexChanged += new System.EventHandler(this.cmblistId_SelectedIndexChanged);
+            this.cmbx.FormattingEnabled = true;
+            this.cmbx.Location = new System.Drawing.Point(209, 174);
+            this.cmbx.Name = "cmbx";
+            this.cmbx.Size = new System.Drawing.Size(230, 24);
+            this.cmbx.TabIndex = 5;
+            this.cmbx.SelectedIndexChanged += new System.EventHandler(this.cmblistId_SelectedIndexChanged);
             // 
             // btnEditPrice
             // 
             this.btnEditPrice.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnEditPrice.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditPrice.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditPrice.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditPrice.ForeColor = System.Drawing.Color.White;
-            this.btnEditPrice.Location = new System.Drawing.Point(161, 202);
+            this.btnEditPrice.Location = new System.Drawing.Point(209, 305);
             this.btnEditPrice.Name = "btnEditPrice";
-            this.btnEditPrice.Size = new System.Drawing.Size(271, 43);
+            this.btnEditPrice.Size = new System.Drawing.Size(230, 43);
             this.btnEditPrice.TabIndex = 3;
-            this.btnEditPrice.Text = "Edit ";
+            this.btnEditPrice.Text = "Edit Item Price";
             this.btnEditPrice.UseVisualStyleBackColor = false;
+            this.btnEditPrice.Click += new System.EventHandler(this.btnEditPrice_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11F);
-            this.label1.Location = new System.Drawing.Point(20, 144);
+            this.label1.Location = new System.Drawing.Point(14, 249);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(104, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "Item Price";
             // 
-            // lblcomboTitle
+            // btnCloseApp
             // 
-            this.lblcomboTitle.AutoSize = true;
-            this.lblcomboTitle.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblcomboTitle.Location = new System.Drawing.Point(478, 22);
-            this.lblcomboTitle.Name = "lblcomboTitle";
-            this.lblcomboTitle.Size = new System.Drawing.Size(231, 22);
-            this.lblcomboTitle.TabIndex = 4;
-            this.lblcomboTitle.Text = "Select an id to edit price";
+            this.btnCloseApp.BackColor = System.Drawing.Color.Red;
+            this.btnCloseApp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCloseApp.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloseApp.ForeColor = System.Drawing.Color.White;
+            this.btnCloseApp.Location = new System.Drawing.Point(209, 381);
+            this.btnCloseApp.Name = "btnCloseApp";
+            this.btnCloseApp.Size = new System.Drawing.Size(230, 41);
+            this.btnCloseApp.TabIndex = 6;
+            this.btnCloseApp.Text = "Close Application";
+            this.btnCloseApp.UseVisualStyleBackColor = false;
+            this.btnCloseApp.Click += new System.EventHandler(this.btnCloseApp_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11F);
+            this.label2.Location = new System.Drawing.Point(14, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Item Id";
             // 
             // EditStockPriceView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lblcomboTitle);
+            this.Controls.Add(this.btnCloseApp);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEditPrice);
-            this.Controls.Add(this.cmblistId);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmbx);
+            this.Controls.Add(this.txtboxPrice);
             this.Controls.Add(this.lblTitle);
             this.Name = "EditStockPriceView";
-            this.Size = new System.Drawing.Size(731, 313);
+            this.Size = new System.Drawing.Size(663, 510);
             this.Load += new System.EventHandler(this.EditStockPriceView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -117,10 +135,11 @@ namespace StockProgram.UserControls
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox cmblistId;
+        private System.Windows.Forms.TextBox txtboxPrice;
+        private System.Windows.Forms.ComboBox cmbx;
         private System.Windows.Forms.Button btnEditPrice;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblcomboTitle;
+        private System.Windows.Forms.Button btnCloseApp;
+        private System.Windows.Forms.Label label2;
     }
 }
--- UserControls/EditStockPriceView.cs
+++ UserControls/EditStockPriceView.cs
@@ -12,38 +12,91 @@ using System.Configuration;
 
 namespace StockProgram.UserControls
 {
+#pragma warning disable CS1591 // Edit Item Price 
     public partial class EditStockPriceView : UserControl
+#pragma warning restore CS1591 // Item will selected by using combobox 
     {
-        public static SqlConnection myConnection = new SqlConnection(@"Data Source=CSLID;Initial Catalog=ProductCombobox;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+#pragma warning disable CS1591 // Connection String to the Local Database//Replace to your local connection
+        public static SqlConnection con = new SqlConnection(@"Data Source=CSLID;Initial Catalog=ProductCombobox;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+#pragma warning restore CS1591 // 'con' is the name of our connection string t
 
+#pragma warning disable CS1591 // Constructor
         public EditStockPriceView()
+#pragma warning restore CS1591 // Initialize Component
         {
             InitializeComponent();
         }
         private void EditStockPriceView_Load(object sender, EventArgs e)
         {
-            myConnection.Open();
-            SqlCommand myCommand = myConnection.CreateCommand();
-            myCommand.CommandType = CommandType.Text;
-            myCommand.CommandText = "Select ProdPrice from [pdr].tProducts ";
-            myCommand.ExecuteNonQuery();
-            DataTable myDataTable = new DataTable();
-            SqlDataAdapter myDataAdapter = new SqlDataAdapter();
-            myDataAdapter.Fill(myDataTable);
-            foreach (DataRow dataRow in myDataTable.Rows)
-            {
-                cmblistId.Items.Add(dataRow["Id"].ToString());
-            }
-
-
-            myConnection.Close();
+            DataConnection();
         }
 
         private void cmblistId_SelectedIndexChanged(object sender, EventArgs e)
         {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "select ProdPrice from [pdr].tProducts where ProductId='" + cmbx.SelectedItem.ToString() + "'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    txtboxPrice.Text = (dr["ProdPrice"].ToString());
+
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("SelectIndexCombobox threw an exception", ex);
+            }
+        }
+
+
+        private void btnCloseApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+#pragma warning disable CS1591 // Method to connect to the database
+        public void DataConnection()
+#pragma warning restore CS1591 // code to fetch Id from table to the combobox
+        {
+    //Connection to the DataBase using SQL-Server
+
+            #region
+            cmbx.Items.Clear();
+            con.Open();
+            SqlCommand myCommand = con.CreateCommand();
+            myCommand.CommandType = CommandType.Text;
+            myCommand.CommandText = "Select ProductId from [pdr].tProducts ";
+            myCommand.ExecuteNonQuery();
+            DataTable myDataTable = new DataTable();
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter(myCommand);
+            myDataAdapter.Fill(myDataTable);
+            foreach (DataRow dataRow in myDataTable.Rows)
+            {
+                cmbx.Items.Add(dataRow["ProductId"].ToString());
+            }
+            con.Close();
+            #endregion
+
 
         }
 
-        
+        private void ClearRow()
+        {
+            txtboxPrice.Text = "";
+        }
+
+        private void btnEditPrice_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
